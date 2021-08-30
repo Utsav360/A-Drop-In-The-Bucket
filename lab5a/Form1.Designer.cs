db26bd88822c5925c2f38543da11be7762cb2087
Utsavkumar Patel <71225073+Utsav360@users.noreply.github.com>
2021-08-30 17:39:07 -0400

    Add files via upload

---
+++ lab5a/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace lab5a
+{
+    partial class DrawForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DrawForm));
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.ClockTimer = new System.Windows.Forms.Timer(this.components);
+            this.ColourButton = new System.Windows.Forms.Button();
+            this.TrackBar = new System.Windows.Forms.TrackBar();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ClockTimer
+            // 
+            this.ClockTimer.Enabled = true;
+            this.ClockTimer.Interval = 1000;
+            this.ClockTimer.Tick += new System.EventHandler(this.dropWaterFill);
+            // 
+            // ColourButton
+            // 
+            this.ColourButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ColourButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ColourButton.Location = new System.Drawing.Point(26, 24);
+            this.ColourButton.Name = "ColourButton";
+            this.ColourButton.Size = new System.Drawing.Size(110, 32);
+            this.ColourButton.TabIndex = 0;
+            this.ColourButton.Text = "Colour";
+            this.ColourButton.UseVisualStyleBackColor = false;
+            this.ColourButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TrackBar
+            // 
+            this.TrackBar.Location = new System.Drawing.Point(26, 105);
+            this.TrackBar.Name = "TrackBar";
+            this.TrackBar.Size = new System.Drawing.Size(133, 69);
+            this.TrackBar.TabIndex = 2;
+            this.TrackBar.Scroll += new System.EventHandler(this.TrackBar_Scroll);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.CloseButton.Location = new System.Drawing.Point(171, 25);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(110, 31);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.PictureBox.ErrorImage = ((System.Drawing.Image)(resources.GetObject("PictureBox.ErrorImage")));
+            this.PictureBox.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox.Image")));
+            this.PictureBox.Location = new System.Drawing.Point(26, 180);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(133, 103);
+            this.PictureBox.TabIndex = 2;
+            this.PictureBox.TabStop = false;
+            this.PictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // DrawForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(518, 642);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.TrackBar);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.ColourButton);
+            this.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Name = "DrawForm";
+            this.Text = "A Drop in the Bucket";
+            this.TransparencyKey = System.Drawing.Color.White;
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Timer ClockTimer;
+        private System.Windows.Forms.Button ColourButton;
+        private System.Windows.Forms.TrackBar TrackBar;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.PictureBox PictureBox;
+    }
+}
+
---
+++ lab5a/Form1.cs
@@ -0,0 +1,217 @@
+﻿//system libraries
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+
+/*
+
+ 
+
+ @ author Utsavkumar M Patel , Student Number : 000820474 ,
+ 
+ This program was created on the th November 11th December 2020.
+ The primary goal of this program is to make a windows form application of the drop in the bucket which make use of 
+ the graphical interface , Graphics object , Timer , Trackbar control and ColorDialog control.
+
+ (Statement of the authorships) : 
+ I, Utsavkumar M Patel , Student Number 000820474 , certify that this material is my original work.
+ And No other person's work has been used without due acknowledgement.
+
+
+*/
+
+//namespace lab5a
+namespace lab5a
+{
+    
+    /// <summary>
+    /// DrawForm  Class
+    /// </summary>
+    public partial class DrawForm : Form
+    {
+        
+        // local variable initilization
+        private int starter = 1;
+        
+        //setting bucketfull condition to false...
+        bool bucketIsFull = false;
+        
+        //Color of water set to lightblue
+        Color color = Color.LightBlue; 
+       
+        /// <summary>
+        /// DrawForm Class inialization.
+        /// </summary>
+        
+        public DrawForm()
+        {
+           
+            InitializeComponent();
+
+            //set trackbar value to 0.
+            TrackBar.Value = 0;
+
+            base.Paint += DrawBucketContainer;  
+            
+        }
+
+        
+     
+        /// <summary>
+        /// Color button Click method..
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //initialize the colordialog ...
+            ColorDialog clgb = new ColorDialog();
+
+            //Show color option of DialogBox Result..
+            if (clgb.ShowDialog() == DialogResult.OK)
+                  color = clgb.Color;     
+
+        }
+
+
+        /// <summary>
+        /// This method is used to display waterfill.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        
+        private void dropWaterFill(object sender, EventArgs e)
+        {
+
+            // SolidBrush object created 
+            SolidBrush b = new SolidBrush(color);
+
+            //Graphics object created...
+            Graphics g = CreateGraphics();
+            
+            // Using Graphics Fill Rectangle bucket...
+            g.FillRectangle(b, 81, 201, 15, 201 - starter);
+
+
+            if (this.starter >= 100)
+            {
+                //Check if bucket is full or not ...
+                bucketIsFull = true;
+                //Disable the ClockTimer...
+                ClockTimer.Enabled = false;
+                g.FillRectangle(new SolidBrush(BackColor), 81, 201, 15, 201);
+                //Set Trackbar value to 0
+                TrackBar.Value = 0;
+                this.starter = 0;
+            }
+
+            g.FillRectangle(b, 86, 400 - this.starter, 200, 1);
+
+            this.starter = starter + 1;
+
+        }
+
+        /// <summary>
+        /// DrawBucketContainer Method to DrawBucket Object,,...
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="paintEvent"></param>
+        private void DrawBucketContainer(object a, PaintEventArgs paintEvent)
+        {
+
+            //Create graphics object using paintEvent Graphics...
+            Graphics grapics = paintEvent.Graphics;
+
+            //Draw line 1 of container via pen object
+            grapics.DrawLine(new Pen(Color.Gray), 82, 288, 82, 398);
+
+            //Draw line 2 of container via pen object
+            grapics.DrawLine(new Pen(Color.Gray), 82, 398, 283, 398);
+            
+            //Draw line 3 of container via pen object
+            grapics.DrawLine(new Pen(Color.Gray), 283, 398, 283, 288);
+
+
+
+        }
+
+
+
+
+
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+           
+            Application.Exit();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TrackBar_Scroll(object sender, EventArgs e)
+        {
+
+            var speed = (TrackBar.Maximum - TrackBar.Value) * 10;
+
+            if (TrackBar.Value == 10)  //prevent 0 speed when max value
+            {
+                speed = (int)(0.5 * 10);
+            }
+
+            Graphics graphics = CreateGraphics();
+
+            if (TrackBar.Value == 0)
+            {
+                ClockTimer.Enabled = false;
+
+            }
+
+            else
+            {
+
+                if (bucketIsFull == false)
+                {
+                    ClockTimer.Interval = speed;
+                    ClockTimer.Enabled = true;
+                }
+
+                else
+                {
+                    bucketIsFull = false;
+                    graphics.FillRectangle(new SolidBrush(BackColor), 86, 285, 199, 140);
+                    graphics.FillRectangle(new SolidBrush(BackColor), 86, 200, 15, 86);
+                    ClockTimer.Interval = speed;
+                    ClockTimer.Enabled = true;
+                }
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Panel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+
+        }
+    }
